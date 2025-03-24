92f50e709469ab363f745a48e087cdedc32fcca5
S25130 <s25130@pjwstk.edu.pl>
2025-03-24 18:53:56 +0100

    Create Liquid.cs

---
+++ S25130/Liquid.cs
@@ -0,0 +1,26 @@
+namespace S25130;
+
+class Liquid : Container, IHazardNotifier
+{
+    public bool IsHazardous { get; }
+
+    public Liquid(bool isHazardous) : base("L")
+    {
+        IsHazardous = isHazardous;
+        MaxLoad = 1000;
+    }
+    
+    public override void Load(double weight)
+    {
+        double maxAllowedLoad = IsHazardous ? MaxLoad * 0.5 : MaxLoad * 0.9;
+        if (weight > maxAllowedLoad)
+            NotifyHazard("Attempted to overload hazardous liquid container");
+        
+        base.Load(weight);
+    }
+    
+    public void NotifyHazard(string message)
+    {
+        Console.WriteLine($"Hazard Alert [{SerialNumber}]: {message}");
+    }
+}
