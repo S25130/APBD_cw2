7d5329124696416544a22856d9fb0801ebd166db
S25130 <s25130@pjwstk.edu.pl>
2025-03-24 19:24:27 +0100

    Update Program.cs

--- S25130/Program.cs
+++ S25130/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using S25130;
 
-Console.WriteLine("Hello, World!");
+static void Main()
+{
+    Ship ship = new Ship("Neptun", 10, 100000, 20);
+    Liquid milk = new Liquid(false);
+    Gas helium = new Gas(25);
+    Refrigerated banana = new Refrigerated("Fruit", -9.6);
+
+    try
+    {
+        milk.Load(500);
+        helium.Load(1000);
+        banana.Load(1500);
+
+        ship.LoadContainer(milk);
+        ship.LoadContainer(helium);
+        ship.LoadContainer(banana);
+
+        ship.PrintInfo();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
