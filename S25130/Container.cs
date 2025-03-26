7cbc333d8bdf0ce7dc951e8b65bc4aed8bba1799
S25130 <s25130@pjwstk.edu.pl>
2025-03-26 19:30:39 +0100

    Update Container.cs
    Container type counter

--- S25130/Container.cs
+++ S25130/Container.cs
@@ -2,14 +2,18 @@ namespace S25130;
 
 abstract class Container
 {
-    private static int counter = 1;
+    private static Dictionary<string, int> counters = new Dictionary<string, int>()
+    {
+        { "L", 1 }, { "G", 1 }, { "R", 1 }
+    };
+    
     public string SerialNumber { get; }
     public double MaxLoad { get; protected set; }
     public double CurrentLoad { get; protected set; }
     
     protected Container(string type)
     {
-        SerialNumber = $"KON-{type}-{counter++}";
+        SerialNumber = $"KON-{type}-{counters[type]++}";
     }
     
     public virtual void Load(double weight)
